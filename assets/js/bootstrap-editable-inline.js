cc536a1dcd4e7b01d6dbdf8877f13c362f53aa1d
Antonio Ramirez <antonio@clevertech.biz>
2012-12-11 09:29:46 +0100

    Quick-fix for editable (upgrade files for x-editable to support Bootstrap 2.2.0

--- assets/js/bootstrap-editable-inline.js
+++ assets/js/bootstrap-editable-inline.js
@@ -2659,95 +2659,63 @@ Editableform based on Twitter Bootstrap
     
 }(window.jQuery));
 /**
-* Editable Popover 
+* Editable Inline 
 * ---------------------
-* requires bootstrap-popover.js
 */
 (function ($) {
 
     //extend methods
     $.extend($.fn.editableContainer.Constructor.prototype, {
-        containerName: 'popover',
-        //for compatibility with bootstrap <= 2.2.1 (content inserted into <p> instead of directly .popover-content) 
-        innerCss: $($.fn.popover.defaults.template).find('p').length ? '.popover-content p' : '.popover-content',
-
+        containerName: 'editableform',
+        innerCss: null,
+                 
         initContainer: function(){
-            $.extend(this.containerOptions, {
-                trigger: 'manual',
-                selector: false,
-                content: ' '
-            });
-            this.call(this.containerOptions);
-        },        
+            //no init for container
+            //only convert anim to miliseconds (int)
+            if(!this.options.anim) {
+                this.options.anim = 0;
+            }         
+        },
         
-        setContainerOption: function(key, value) {
-            this.container().options[key] = value; 
-        },               
-
-        /**
-        * move popover to new position. This function mainly copied from bootstrap-popover.
-        */
-        /*jshint laxcomma: true*/
-        setPosition: function () { 
-         
-            (function() {    
-                var $tip = this.tip()
-                , inside
-                , pos
-                , actualWidth
-                , actualHeight
-                , placement
-                , tp;
-
-                placement = typeof this.options.placement === 'function' ?
-                this.options.placement.call(this, $tip[0], this.$element[0]) :
-                this.options.placement;
-
-                inside = /in/.test(placement);
-               
-                $tip
-              //  .detach()
-              //vitalets: remove any placement class because otherwise they dont influence on re-positioning of visible popover
-                .removeClass('top right bottom left')
-                .css({ top: 0, left: 0, display: 'block' });
-              //  .insertAfter(this.$element);
-               
-                pos = this.getPosition(inside);
-
-                actualWidth = $tip[0].offsetWidth;
-                actualHeight = $tip[0].offsetHeight;
-
-                switch (inside ? placement.split(' ')[1] : placement) {
-                    case 'bottom':
-                        tp = {top: pos.top + pos.height, left: pos.left + pos.width / 2 - actualWidth / 2};
-                        break;
-                    case 'top':
-                        tp = {top: pos.top - actualHeight, left: pos.left + pos.width / 2 - actualWidth / 2};
-                        break;
-                    case 'left':
-                        tp = {top: pos.top + pos.height / 2 - actualHeight / 2, left: pos.left - actualWidth};
-                        break;
-                    case 'right':
-                        tp = {top: pos.top + pos.height / 2 - actualHeight / 2, left: pos.left + pos.width};
-                        break;
-                }
-
-                $tip
-                .offset(tp)
-                .addClass(placement)
-                .addClass('in');
-                
-            }).call(this.container());
-          /*jshint laxcomma: false*/  
-        }            
+        splitOptions: function() {
+            this.containerOptions = {};
+            this.formOptions = this.options;
+        },
+        
+        tip: function() {
+           return this.$form; 
+        },
+        
+        innerShow: function () {
+            this.$element.hide();
+            
+            if(this.$form) {
+                this.$form.remove();
+            }
+            
+            this.initForm();
+            this.tip().addClass('editable-container').addClass('editable-inline');            
+            this.$form.insertAfter(this.$element);
+            this.$form.show(this.options.anim);
+            this.$form.editableform('render');
+        }, 
+        
+        innerHide: function () {
+            this.$form.hide(this.options.anim, $.proxy(function() {
+                this.$element.show();
+            }, this)); 
+        },
+        
+        destroy: function() {
+            this.tip().remove();
+        } 
     });
 
     //defaults
-    /*
-    $.fn.editableContainer.defaults = $.extend({}, $.fn.popover.defaults, $.fn.editableContainer.defaults, {
-        
-    });
-    */    
+    $.fn.editableContainer.defaults = $.extend({}, $.fn.editableContainer.defaults, {
+        anim: 'fast'
+    });    
+
 
 }(window.jQuery));
 /**
