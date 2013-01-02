a3b8a28c8e06aecb48398a0e95548657ad7813cb
kanyuga <kanyuga@gmail.com>
2013-01-02 11:24:33 +0300

    Renamed TbButtonGroupColumn to TbButtonGroupColumn.php

--- widgets/TbButtonGroupColumn.php
+++ widgets/TbButtonGroupColumn.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * TbGroupButtonColumn class file.
+ * TbButtonGroupColumn class file.
  * @author Topher Kanyuga <kanjoti@gmail.com>
  * @copyright  
  * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
