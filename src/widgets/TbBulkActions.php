759a5d57a6ee86a87412dfb438af9ca4b04db63d
james <jamesmbowler@gmail.com>
2013-05-11 19:11:49 -0700

    implement compatibility for selectableRows

--- src/widgets/TbBulkActions.php
+++ src/widgets/TbBulkActions.php
@@ -41,6 +41,16 @@ class TbBulkActions extends CComponent
      * @var array the checkbox column configuration
      */
     public $checkBoxColumnConfig = array();
+    
+    /**
+	 * @var bool
+	 */
+	public $selectableRows;
+	
+	/**
+	 * @var string
+	 */
+	public $noCheckedMessage = 'No items are checked';
 
     /**
      * @var string
@@ -223,7 +233,11 @@ class TbBulkActions extends CComponent
      */
     public function registerClientScript()
     {
-        $js = <<<EOD
+       
+		$js = '';
+		if(!$this->selectableRows)
+		{
+		$js .= <<<EOD
 $(document).on("click", "#{$this->grid->id} input[type=checkbox]", function(){
 	var grid = $("#{$this->grid->id}");
 	if ($("input[name='{$this->columnName}']:checked", grid).length)
@@ -239,8 +253,17 @@ $(document).on("click", "#{$this->grid->id} input[type=checkbox]", function(){
 	}
 });
 EOD;
+}
         foreach ($this->events as $buttonId => $handler) {
-            $js .= "\n$(document).on('click','#{$buttonId}', function(){var checked = $('input[name=\"{$this->columnName}\"]:checked');\n
+            $js .= "\n$(document).on('click','#{$buttonId}', function(){
+			
+			var grid = $(\"#{$this->grid->id}\");
+			if (!$(\"input[name='{$this->columnName}']:checked\", grid).length)
+			{
+				alert('".$this->noCheckedMessage."');
+				return false;
+			}
+				var checked = $('input[name=\"{$this->columnName}\"]:checked');\n
 			var fn = $handler; if ($.isFunction(fn)){fn(checked);}\nreturn false;});\n";
         }
         Yii::app()->getClientScript()->registerScript(__CLASS__ . '#' . $this->getId(), $js);
@@ -256,11 +279,13 @@ EOD;
     protected function renderButton($actionButton)
     {
         // create widget and display
-        if (isset($actionButton['htmlOptions']['class'])) {
-            $actionButton['htmlOptions']['class'] .= ' disabled bulk-actions-btn';
-        } else {
-            $actionButton['htmlOptions']['class'] = 'disabled bulk-actions-btn';
-        }
+		if (isset($actionButton['htmlOptions']['class']) && !$this->selectableRows)
+			$actionButton['htmlOptions']['class'] .= ' disabled bulk-actions-btn';
+		elseif (isset($actionButton['htmlOptions']['class']) && $this->selectableRows)
+			$actionButton['htmlOptions']['class'] .= 'bulk-actions-btn';
+		else
+			$actionButton['htmlOptions']['class'] = 'disabled bulk-actions-btn';
+		$action = null;
 
         $action = null;
 
