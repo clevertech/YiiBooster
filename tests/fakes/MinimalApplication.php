765c73cc3336e256b67b592c1cb05fa55c4bca72
Saphronov Mark <hijarian@gmail.com>
2013-06-07 16:58:26 +0400

    Added annotations for phpStorm

--- tests/fakes/MinimalApplication.php
+++ tests/fakes/MinimalApplication.php
@@ -3,6 +3,8 @@
  * Class MinimalApplication
  * Stub to support characterization tests.
  * It's instantiated in bootstrap script for phpunit.
+ *
+ * @property Bootstrap bootstrap
  */
 class MinimalApplication extends CWebApplication {
 
