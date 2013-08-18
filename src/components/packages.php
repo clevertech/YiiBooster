7df2b390d6e8b3d55e5d1388f50ee5c5b447009a
miramir <gmiramir@gmail.com>
2013-08-18 17:51:09 +0600

    refactoring

--- src/components/packages.php
+++ src/components/packages.php
@@ -12,16 +12,12 @@
 return array(
 
 	'font-awesome' => array(
-		'baseUrl' => $this->enableCdn ? '//netdna.bootstrapcdn.com/font-awesome/3.2.1/' : $this->getAssetsUrl(),
-		'css' => array($this->minifyCss 
-						? ($this->enableCdn?'font-awesome/':'').'css/font-awesome.min.css' 
-						: ($this->enableCdn?'font-awesome/':'').'css/font-awesome.css'),
+		'baseUrl' => $this->enableCdn ? '//netdna.bootstrapcdn.com/font-awesome/3.2.1/' : $this->getAssetsUrl().'/font-awesome/',
+		'css' => array($this->minifyCss ? 'css/font-awesome.min.css' : 'css/font-awesome.css'),
 	),
 	'font-awesome-ie7' => array(
-		'baseUrl' => $this->enableCdn ? '//netdna.bootstrapcdn.com/font-awesome/3.2.1/' : $this->getAssetsUrl(),
-		'css' => array($this->minifyCss 
-						? ($this->enableCdn?'font-awesome/':'').'css/font-awesome-ie7.min.css' 
-						: ($this->enableCdn?'font-awesome/':'').'css/font-awesome-ie7.css'),
+		'baseUrl' => $this->enableCdn ? '//netdna.bootstrapcdn.com/font-awesome/3.2.1/' : $this->getAssetsUrl().'/font-awesome/',
+		'css' => array($this->minifyCss ? 'css/font-awesome-ie7.min.css' : 'css/font-awesome-ie7.css'),
 	),
 	'bootstrap.js' => array(
 		'baseUrl' => $this->enableCdn ? '//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/' : $this->getAssetsUrl() . '/bootstrap/',
