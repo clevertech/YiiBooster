c08a6ef082d870974ded98b8de4ebc9b5e27ce84
amrbedair <amr.bedair@gmail.com>
2014-05-19 19:38:41 +0200

    #811

--- src/assets/js/bootstrap-noconflict.js
+++ src/assets/js/bootstrap-noconflict.js
@@ -3,4 +3,15 @@ var bootstrapButton, bootstrapTooltip;
 (function ($) {
     bootstrapButton = $.fn.button;
     bootstrapTooltip = $.fn.tooltip;
+    
+    /* fix for #811 based on suggested solution at https://github.com/twbs/bootstrap/issues/4550 */
+	$('.dropdown a').click(function(e) {
+		e.preventDefault();
+		setTimeout($.proxy(function() {
+			if ('ontouchstart' in document.documentElement) {
+				$(this).siblings('.dropdown-backdrop').off().remove();
+			}
+		}, this), 0);
+	});
+
 })(jQuery);
